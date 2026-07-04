0144f11f578c82a9d9a27bc5d91e6d185f40a610
Daniel Morón <94260698+danielmr6@users.noreply.github.com>
2026-07-04 09:27:41 +0200

    google endpoint allowed

--- backend/socialhabits/src/main/java/com/unir/socialhabits/config/SecurityConfig.java
+++ backend/socialhabits/src/main/java/com/unir/socialhabits/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/smtp-test"
+                                "/smtp-test",
+                                "/google"
                         ).permitAll()
 
                         // PRE-FLIGHT REQUESTS
